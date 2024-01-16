2a1eb81508ecb58f2b10255b2254a35a6d0b62eb
Titus Kovalik <titus.kovalik@tlgcohort.com>
2024-01-15 21:22:12 -0800

    minimal viable product complete. Takes and saves notes to a user profile.

--- week1_proj.py
+++ week1_proj.py
@@ -1,24 +1,49 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 
+
 conn_str = "mongodb+srv://GreenSpaceman22:GreenGoose22!@stickynoteapp.7vdetmi.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(conn_str)
 db = client['stickynoteapp']
 collection = db['user_note_app']
-
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
 @app.post('/post_it')
 def post_it():
+    cursor = collection.find()
     data = request.json
-    print(data)
-
-    result = collection.insert_one(data)
-
-    return jsonify({'message': 'Data saved successfully', 'id': str(result.inserted_id)})
-
+    user_id = data.get("user_name")
+    user_id = user_id.replace(" ", "")
+    notes = data.get("Note")
+    notes = f"{notes}"
+    count_documents = collection.count_documents({})
+    user_to_find = {'user_name' : f'{user_id}'}
+      
+    if count_documents == 0:
+        collection.insert_one({'user_name' : user_id, 'Note' : notes})
+        with open(f'{user_id}.txt', 'w') as new_user_notes:
+            new_user_notes.write(notes)
+        
+    else: 
+        user_list = []
+        for document in cursor:
+            user_list.append(document.get('user_name'))
+        if user_id in user_list:
+            with open(f'{user_id}.txt', 'a') as user_notes:
+                user_notes.write(notes)
+                user_notes.close()
+            with open(f'{user_id}.txt', 'r') as user_notes_read:
+                new_notes = user_notes_read.read()
+                update_data = {'$set' : {'user_name' : user_id, 'Note' : new_notes}}
+                collection.update_one(user_to_find, update_data) 
+        else:
+            with open(f'{user_id}.txt', 'w') as new_user_notes:
+                new_user_notes.write(notes)
+                collection.insert_one({'user_name' : user_id, 'Note' : notes})   
+       
+    return jsonify({user_id : notes}) 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
